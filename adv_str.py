ec504f714cc2458283e3eaece466394aaac73f67
lucastimho <lucas.t.ho.lh@gmail.com>
2022-04-11 16:46:39 -0500

    addds advanced string methods

---
+++ adv_str.py
@@ -0,0 +1,20 @@
+s = "hello world"
+print(s.capitalize())
+print(s.upper())
+print(s.lower())
+print(s.count("o"))
+print(s.find("o"))
+print(s.center(20, "z"))
+"hello/thi".expandtabs()
+s = "hello"
+print(s.isalnum())
+print(s.isalpha())
+print(s.islower())
+print(s.isspace())
+print(s.istitle())
+print(s.isupper())
+print(s.endswith("o"))
+print(s.split("e"))
+s = "hihhiiihiiihihh"
+print(s.split("i"))
+print(s.partition("i"))
